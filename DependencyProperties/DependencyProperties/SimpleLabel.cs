6a6445c6c75834b673491e3f3d6453dc0ef4e902
Jakob Ferdinand Wegenschimmel <j.wegenschimmel@gmail.com>
2017-11-29 13:58:51 +0100

    FontSizeProperty

---
+++ DependencyProperties/DependencyProperties/SimpleLabel.cs
@@ -0,0 +1,29 @@
+﻿using System.Windows;
+
+namespace DependencyProperties
+{
+    public class SimpleLabel : FrameworkElement
+    {
+        public static readonly DependencyProperty fontSizeProperty;
+
+        static SimpleLabel()
+        {
+            var meta = new FrameworkPropertyMetadata(
+                defaultValue: 11.0,
+                flags: FrameworkPropertyMetadataOptions.AffectsMeasure | FrameworkPropertyMetadataOptions.AffectsRender);
+
+            fontSizeProperty = DependencyProperty.Register(
+                name: nameof(FontSize),
+                propertyType: typeof(double),
+                ownerType: typeof(SimpleLabel),
+                typeMetadata: meta,
+                validateValueCallback: v => (double)v > 1);
+        }
+
+        public double FontSize
+        {
+            get => (double)GetValue(fontSizeProperty);
+            set => SetValue(fontSizeProperty, value);
+        }
+    }
+}
