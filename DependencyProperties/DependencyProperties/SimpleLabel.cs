896c0f845bdea4bc0143c265a8db61ca96420469
Jakob Ferdinand Wegenschimmel <j.wegenschimmel@gmail.com>
2017-11-29 14:02:25 +0100

    Text Property

--- DependencyProperties/DependencyProperties/SimpleLabel.cs
+++ DependencyProperties/DependencyProperties/SimpleLabel.cs
@@ -5,6 +5,7 @@ namespace DependencyProperties
     public class SimpleLabel : FrameworkElement
     {
         public static readonly DependencyProperty fontSizeProperty;
+        public static readonly DependencyProperty textProperty;
 
         static SimpleLabel()
         {
@@ -18,12 +19,25 @@ namespace DependencyProperties
                 ownerType: typeof(SimpleLabel),
                 typeMetadata: meta,
                 validateValueCallback: v => (double)v > 1);
+
+            textProperty = DependencyProperty.Register(
+                nameof(Text),
+                typeof(string),
+                typeof(SimpleLabel),
+                new FrameworkPropertyMetadata(
+                    string.Empty,
+                    FrameworkPropertyMetadataOptions.AffectsMeasure | FrameworkPropertyMetadataOptions.AffectsRender));
         }
 
         public double FontSize
         {
             get => (double)GetValue(fontSizeProperty);
             set => SetValue(fontSizeProperty, value);
+        }
+        public string Text
+        {
+            get => (string)GetValue(textProperty);
+            set => SetValue(textProperty, value);
         }
     }
 }
