51ee59a29b9cc48839da6b8eeba96449e60672b3
Jakob Ferdinand Wegenschimmel <j.wegenschimmel@gmail.com>
2017-11-29 09:20:07 +0100

    Multi Value Converter

---
+++ MultiBindingExample/MultiBindingExample/RgbToBrushConverter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace MultiBindingExample
+{
+    internal class RgbToBrushConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            var a = (byte)(double)values[0];
+            var r = (byte)(double)values[1];
+            var g = (byte)(double)values[2];
+            var b = (byte)(double)values[3];
+
+            return new SolidColorBrush(Color.FromArgb(a, r, g, b));
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            var color = (value as SolidColorBrush).Color;
+            return new object[]
+            {
+                (double)color.A,
+                (double)color.R,
+                (double)color.G,
+                (double)color.B
+            };
+        }
+    }
+}
