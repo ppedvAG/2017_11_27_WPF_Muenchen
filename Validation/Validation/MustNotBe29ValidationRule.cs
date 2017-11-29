4298257ce18d14d546fc7c14982172f5d241383d
Jakob Ferdinand Wegenschimmel <j.wegenschimmel@gmail.com>
2017-11-29 11:38:15 +0100

    Validation Rules

---
+++ Validation/Validation/MustNotBe29ValidationRule.cs
@@ -0,0 +1,12 @@
+﻿using System.Globalization;
+using System.Windows.Controls;
+
+namespace Validation
+{
+    internal class MustNotBe29ValidationRule : ValidationRule
+    {
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo) => value is string s && s == "29"
+            ? new ValidationResult(false, "29 gefällt mir nicht. :P")
+            : ValidationResult.ValidResult;
+    }
+}
